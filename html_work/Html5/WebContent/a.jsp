1d81c0dd8e0cfa56ed6f3fcc764380d1f4d64ed9
gunvv00 <gunvv00@naver.com>
2020-02-14 18:04:19 +0900

    오늘은 여기까지

---
+++ html_work/Html5/WebContent/a.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+<% int a = 1; %>
+
+<h1 > <%=  a %></h1>
+<%for(int i = 0; i<10; i++){ %>
+<h1>1</h1>
+<%} %>
+</body>
+</html>
---
+++ html_work/PracticeSelf/src/designSelf/FlexibleCompanyDemoInterfaceVersion2.java
@@ -0,0 +1,95 @@
+package designSelf;
+
+public class FlexibleCompanyDemoInterfaceVersion2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+interface Worker {
+	public String getName();
+	
+	public void setName(String name);
+	
+	public void setSalaryRole(SalaryRole salaryRole);
+	
+	public SalaryRole getSalaryRole();
+	
+	public double getSalary();
+	
+	public void setSalary(double salary);
+	
+	public void manageSalary(double rate);
+}
+
+abstract class HumanWorker1 implements Worker {
+	String name;
+	SalaryRole salaryRole = null;
+	
+	public HumanWorker1(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public double getSalary() {
+		return salaryRole.getSalary();
+	}
+	
+	public void setSalary(double salary) {
+		salaryRole.setSalary(salary);
+	}
+	
+	public void manageSalary(double rate) {
+		salaryRole.manageSalary(rate);
+	}
+	
+	public void setSalaryRole(SalaryRole salaryRole) {
+		this.salaryRole = salaryRole;
+	}
+	
+	public SalaryRole getSalaryRole() {
+		return salaryRole;
+	}
+}
+
+class ManWorker extends HumanWorker1  {
+	public ManWorker(String name) {
+		super(name);
+	}
+	public void doShaving() {
+		System.out.println("면도를 한다.");
+	}
+	
+}
+
+class WomanWorker extends HumanWorker1 {
+	public WomanWorker(String name) {
+		super(name);
+	}
+	public void doMakeup() {
+		System.out.println("화장을 한다.");
+	}
+}
+
+
+abstract class Animal implements Worker {
+	Sting name
+}
+
+
+
+
+
+
+
+
